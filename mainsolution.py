10a4b37b9ed3d58f5edc6c4347e0ef3a2187fcef
brankf <108978938+brankf@users.noreply.github.com>
2022-07-28 14:26:06 +0800

    removed unnecessary lines

--- mainsolution.py
+++ mainsolution.py
@@ -1,7 +1,1 @@
 import api, cash_on_hand, overheads, profit_loss
-print("hello world")
-print("slayyy")
-print("hello brandon")
-print("bye brandon")
-print("i am at starbucks")
-print("i am at home")
