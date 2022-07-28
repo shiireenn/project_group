3d67621d779542fccc04ed67b4898a927e38807f
shiireenn <shiireenn@gmail.com>
2022-07-28 15:48:50 +0800

    profit commit 1

--- profit_loss.py
+++ profit_loss.py
@@ -1,1 +1,8 @@
-profit and loss
+#from pathlib import Path
+#import csv
+#
+#file_path = Path.cwd()/"csv_reports"/"profit_loss.csv"
+#
+#with file_path.open(mode = "r", encoding = "UTF-8") as file:
+#    read = csv.reader(file)
+#    print(read)
