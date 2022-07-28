a691e47b6a477eab7db334f296d3ed5999519584
brankf <108978938+brankf@users.noreply.github.com>
2022-07-28 15:05:08 +0800

    copied codes from assignment brief

--- mainsolution.py
+++ mainsolution.py
@@ -3,4 +3,9 @@ import api, cash_on_hand, overheads, profit_loss
 
 def main():
 
-    forex = api.api_function
+    forex = api.api_function()
+    overheads.overheads_function(forex)
+    cash_on_hand.cash_on_hand(forex)
+    profit_loss.profitloss_function(forex)
+
+main()
