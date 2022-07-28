37a8c102b75a360f5e51cac6aa149165436ce6bf
shiireenn <shiireenn@gmail.com>
2022-07-28 22:58:33 +0800

    Read profit and loss csv

--- profit_loss.py
+++ profit_loss.py
@@ -1,8 +1,11 @@
-#from pathlib import Path
-#import csv
-#
-#file_path = Path.cwd()/"csv_reports"/"profit_loss.csv"
-#
-#with file_path.open(mode = "r", encoding = "UTF-8") as file:
-#    read = csv.reader(file)
-#    print(read)
+from pathlib import Path
+import csv
+
+file_path = Path.cwd()/"csv_reports"/"profit_loss.csv"
+
+with file_path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
+    read = csv.reader(file)
+    
+    for line in read:
+        print(line)
+
