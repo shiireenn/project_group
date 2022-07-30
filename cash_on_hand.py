f73ee32cb75691eae60abac287ef7d582f0bfd0d
Zan402 <zandraleeyunjia@gmail.com>
2022-07-30 22:06:51 +0800

    hello

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,2 +1,10 @@
 #zandra
+import csv
 
+full_list = []
+with open(r"C:/Users/zan/Downloads/cash-on-hand-usd.csv", "r") as file:
+    reader = csv.reader(file)
+    next(reader)
+    next(reader)
+    for line in reader:
+        full_list.append(line)
