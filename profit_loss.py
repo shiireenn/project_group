04b8e48bdd7e548ca585b30786389fe1a322fbb9
shiireenn <shiireenn@gmail.com>
2022-07-30 23:54:55 +0800

    Profit trial 2

--- profit_loss.py
+++ profit_loss.py
@@ -1,22 +1,43 @@
 from pathlib import Path
 import csv
 
+# trial 1
+
 file_path = Path.cwd()/"csv_reports"/"profit_loss.csv"
 
-profit_list = []
-day_list = []
+#profit_list = []
+#day_list = []
+#
+#with file_path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
+#    reader = csv.reader(file)
+#    next(reader)
+#    for line in reader:
+#        day_list.append(int(line[0]))
+#        profit_list.append(int(line[4]))
+#
+#print(day_list)
+#print(profit_list)
+
+# trial 2
+
+file_path = Path.cwd()/"csv_reports"/"profit_loss.csv"
+
+empty_list = []
 
 with file_path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
     reader = csv.reader(file)
     next(reader)
+    
     for line in reader:
-        day_list.append(int(line[0]))
-        profit_list.append(int(line[4]))
+        empty_list.append(line)
 
-print(day_list)
-print(profit_list)
+print(empty_list)
 
+for day in range(empty_list):
 
+    if day < day - 1:
+        print("fail")
 
-
+    else:
+        print("success")
 
