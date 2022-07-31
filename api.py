3b98cfd7a228558e97a7f17d2dae498c70d6b73a
shiireenn <shiireenn@gmail.com>
2022-07-31 22:14:44 +0800

    API call to Alpha Vantage

--- api.py
+++ api.py
@@ -1,1 +1,8 @@
-#jayden
+import requests
+
+url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=OX96WV3X7K2KBVZ5"
+
+r = requests.get(url)
+data = r.json()
+
+print(data)
