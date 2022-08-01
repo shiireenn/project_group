e7abd5fff339c6c4db4380d616ad25c66c03e9f1
shiireenn <shiireenn@gmail.com>
2022-08-01 23:00:59 +0800

    Import to API

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
+from pathlib import Path
 import requests
+import json,re
 
 url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey=OX96WV3X7K2KBVZ5"
 
