62002ad5c02a200741a0073eb6799e3e6cb5a111
shiireenn <shiireenn@gmail.com>
2022-08-02 13:53:03 +0800

    Import

--- profit_loss.py
+++ profit_loss.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import csv
+import requests
+import json, re
 
 # trial 1
 
@@ -28,14 +30,15 @@ empty_list = []
 def profitloss_function():
     with file_path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
         reader = csv.reader(file)
+        # Skip the headers
         next(reader)
     
-        for line in reader:
-            line.pop(1)
-            line.pop(1)
-            line.pop(1)
-            float_lst = [float(item) for item in line]
-            empty_list.append(float_lst)
+#        for line in reader:
+#            line.pop(1)
+#            line.pop(1)
+#            line.pop(1)
+#            float_lst = [float(item) for item in line]
+#            empty_list.append(float_lst)
         
-print(empty_list)
+    
 
