3c020c1084c91d80d5ffc99fe9b67377e87bd1e2
Zan402 <zandraleeyunjia@gmail.com>
2022-08-02 15:39:08 +0800

    acd

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -3,10 +3,13 @@ from pathlib import Path
 import csv
 
 full_list = []
-with open(r"./csv_reports/cash_on_hand.csv", "r") as file:
-    reader = csv.reader(file)
-    next(reader)
-    for line in reader:
-        full_list.append(line)
-
-print(full_list)
+def cash_on_hand_function():
+    with open(r"./csv_reports/cash_on_hand.csv", "r") as file:
+        reader = csv.reader(file)
+        next(reader)
+        for line in reader:
+            full_list.append(line)
+        previous_day = 0
+        value_difference = 0
+        
+        
