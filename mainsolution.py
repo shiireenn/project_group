3fe9935b52cc96eaee004433ce11ba83c6bc6069
shiireenn <shiireenn@gmail.com>
2022-08-02 15:09:16 +0800

    mainsolution

--- mainsolution.py
+++ mainsolution.py
@@ -4,7 +4,6 @@ import csv
 import json,re
 import requests
 
-path = Path.cwd()
 # Set current working directory to csv_reports
 cwd_path = Path.cwd()/"csv_reports"
 # Current working directory of summary_report.txt will be csv_report
@@ -12,23 +11,11 @@ summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
 # Touch summary_path to csv_reports
 summary_path.touch()
 
+def main():
+    forex = api.api_function()
+    overheads.overheads_function(forex)
+    cash_on_hand.cash_on_hand(forex)
+    profit_loss.profitloss_function(forex)
 
-cwd_path = Path.cwd()/"PROJECT_GROUP"
-# # cwd_path.exists()
-summary_path = Path.cwd()/"PROJECT_GROUP"/"csv_reports"/"summary_report.txt"
-# # #creating csv file using .touch() method
-summary_path.exists()
-# summary_path.touch()
+#    with summary_path.open(mode = "w", encoding = "UTF-8", newline = ""):
 
-
-
-# def main():
-
-#     forex = api.api_function()
-#     overheads.overheads_function(forex)
-#     cash_on_hand.cash_on_hand(forex)
-#     profit_loss.profitloss_function(forex)
-
-# #    with summary_path.open(mode = "w", encoding = "UTF-8", newline = ""):
-
-# main()
