be68f872958fbd0c85d714800892062b39698bf7
maryslittlelamb <theophania2005@gmail.com>
2022-08-02 23:01:21 +0800

    overheads.py

--- overheads.py
+++ overheads.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import csv
 
 file_path = Path.cwd()/"csv_reports"/"overheads.csv"
+#current working directory of summary_report.txt will be csv_report
 summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
 
 #create a function 'overheads_category' that returns the highest category of overheads
@@ -19,21 +20,21 @@ overheads = {}
 
 #create 'reader' object and print line if file path exists
 with file_path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
-    #instantiate a read object
+#instantiate a read object
     reader = csv.reader(file)
     #use 'next()' to skip Header
     #next(reader)
 
 # Create nested loop to access each value in the list
-# and append the value to the empty_list.
+# and append the value to 'percentage'.
 # Header is skipped due to `next()` before for loop
     for line in reader:
         data = float(file[1])
         category = data[0]
         percentage.append[data]
         highest = max(data)
-        highest_category = max{category}
-        f"([HIGHEST_OVERHEADS] {highest_category.upper()} = {highest} %"
-return message
+        highest_category = {category}
+        message = f"([HIGHEST_OVERHEADS] {highest_category.upper()} = {highest} %"
+        
 
 print(overheads_function())
