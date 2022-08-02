022ac1fef17803742e494f815abe67e317a0fe21
maryslittlelamb <theophania2005@gmail.com>
2022-08-02 23:28:01 +0800

    haha

--- overheads.py
+++ overheads.py
@@ -13,28 +13,27 @@ def overheads_function():
     """
     This function returns the category with the highest overheads in percentage.
     """
-
-#create and assign an empty list to 'percentage'
-percentage = []
-overheads = {}
+    #create and assign an empty list to 'percentage'
+    percentage = []
+    overheads = {}
 
 #create 'reader' object and print line if file path exists
-with file_path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
+    with file_path.open(mode = "r", encoding = "UTF-8", newline = "") as file:
 #instantiate a read object
-    reader = csv.reader(file)
+        reader = csv.reader(file)
     #use 'next()' to skip Header
     #next(reader)
 
 # Create nested loop to access each value in the list
 # and append the value to 'percentage'.
 # Header is skipped due to `next()` before for loop
-    for line in reader:
-        data = float(file[1])
-        category = data[0]
-        percentage.append[data]
-        highest = max(data)
-        highest_category = {category}
-        message = f"([HIGHEST_OVERHEADS] {highest_category.upper()} = {highest} %"
-        
+        for line in reader:
+            data = float(file[1])
+            category = data[0]
+            percentage.append[data]
+            highest = max(data)
+            highest_category = {category}
+            message = f"([HIGHEST_OVERHEADS] {highest_category.upper()} = {highest} %"
+            return message
 
 print(overheads_function())
