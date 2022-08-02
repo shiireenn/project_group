610a32ada034a1bf5a58c7bdec760a866ef82a64
Zan402 <zandraleeyunjia@gmail.com>
2022-08-02 15:23:15 +0800

    hele

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -6,7 +6,6 @@ full_list = []
 with open(r"./csv_reports/cash_on_hand.csv", "r") as file:
     reader = csv.reader(file)
     next(reader)
-    
     for line in reader:
         full_list.append(line)
 
