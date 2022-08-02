8c0cc41b06585294aedf5f49525d03cf5e80a0c9
shiireenn <shiireenn@gmail.com>
2022-08-02 15:49:01 +0800

    Write into txt

--- mainsolution.py
+++ mainsolution.py
@@ -17,4 +17,9 @@ def main():
     cash_on_hand.cash_on_hand(forex)
     profit_loss.profitloss_function(forex)
 
+    with summary_path.open(mode = "w", encoding = "UTF-8", newline = "") as file:
+        for line in main:
+            file.writelines(line)
+            
+        
 
