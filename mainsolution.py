68a22239845b98492890dcacc62431ec12c30978
brankf <108978938+brankf@users.noreply.github.com>
2022-08-02 14:55:58 +0800

    trying

--- mainsolution.py
+++ mainsolution.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import api, cash_on_hand, overheads, profit_loss
+#import api, cash_on_hand, overheads, profit_loss
 
 ## Set current working directory to csv_reports
 #cwd_path = Path.cwd()/"csv_reports"
@@ -8,13 +8,23 @@ import api, cash_on_hand, overheads, profit_loss
 ## Touch summary_path to csv_reports
 #summary_path.touch()
 
-def main():
 
-    forex = api.api_function()
-    overheads.overheads_function(forex)
-    cash_on_hand.cash_on_hand(forex)
-    profit_loss.profitloss_function(forex)
+cwd_path = Path.cwd()/"PROJECT_GROUP"
+# # cwd_path.exists()
+summary_path = Path.cwd()/"PROJECT_GROUP"/"csv_reports"/"summary_report.txt"
+# # #creating csv file using .touch() method
+summary_path.exists()
+# summary_path.touch()
 
-#    with summary_path.open(mode = "w", encoding = "UTF-8", newline = ""):
 
-main()
+
+# def main():
+
+#     forex = api.api_function()
+#     overheads.overheads_function(forex)
+#     cash_on_hand.cash_on_hand(forex)
+#     profit_loss.profitloss_function(forex)
+
+# #    with summary_path.open(mode = "w", encoding = "UTF-8", newline = ""):
+
+# main()
