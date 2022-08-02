3f32a9f40ea78a793b35d28d54b7b9765677a192
shiireenn <shiireenn@gmail.com>
2022-08-02 14:59:23 +0800

    mainsolution

--- mainsolution.py
+++ mainsolution.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 import api, cash_on_hand, overheads, profit_loss
+import csv
+import json,re
+import requests
 
-## Set current working directory to csv_reports
-#cwd_path = Path.cwd()/"csv_reports"
-## Current working directory of summary_report.txt will be csv_report
-#summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
-## Touch summary_path to csv_reports
-#summary_path.touch()
+path = Path.cwd()
+# Set current working directory to csv_reports
+cwd_path = Path.cwd()/"csv_reports"
+# Current working directory of summary_report.txt will be csv_report
+summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
+# Touch summary_path to csv_reports
+summary_path.touch()
 
 def main():
 
