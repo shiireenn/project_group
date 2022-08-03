e9083690280564147bed9b31da2c8b82b16c7d6d
Zan402 <zandraleeyunjia@gmail.com>
2022-08-03 17:40:15 +0800

    co

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -6,6 +6,9 @@ file_path = Path.cwd()/"csv_reports"/"cash_on_hand.csv"
 summary_path = Path.cwd()/"csv_reports"/"summary_report.txt"
 
 def cash_on_hand_function(forex):
+    '''
+    This function highlights the days where cash on hand was lower than the previous day and also shows the value difference
+    '''
     with file_path.open(mode = 'r', encoding = 'UTF-8') as file:
         reader = csv.reader(file)
         previous_day = 0
